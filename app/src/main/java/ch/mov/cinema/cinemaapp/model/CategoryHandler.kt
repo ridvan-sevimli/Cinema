01b92ac01d9b0c6bd43cf5cd8e38aa6e19ec97d0
sevimrid <ridvan-sevimli@hotmail.com>
2022-12-19 04:50:31 +0100

    Refactory CategoryHandler

--- app/src/main/java/ch/mov/cinema/cinemaapp/model/CategoryHandler.kt
+++ app/src/main/java/ch/mov/cinema/cinemaapp/model/CategoryHandler.kt
@@ -1,14 +1,16 @@
 package ch.mov.cinema.cinemaapp.model
 
+import ch.mov.cinema.cinemaapp.model.enums.MainCategoryApi
+
 class CategoryHandler {
 
-    var categoryIdMap = mapOf("https://imdb-api.com/en/API/Top250Movies/k_j38getry" to "top_250_movies",
-        "https://imdb-api.com/en/API/ComingSoon/k_j38getry" to "coming_soon",
-        "https://imdb-api.com/en/API/MostPopularMovies/k_j38getry" to "most_popular_movies",
-        "https://imdb-api.com/en/API/MostPopularTVs/k_j38getry" to "most_popular_tv",
-        "https://imdb-api.com/en/API/InTheaters/k_j38getry" to "in_theaters"
+    var categoryIdMap = mapOf(
+        MainCategoryApi.COMING_SOON.category to "coming_soon",
+        MainCategoryApi.TOP_250_MOVIES.category to "top_250_movies",
+        MainCategoryApi.MOST_POPULAR_MOVIES.category to "most_popular_movies",
+        MainCategoryApi.MOST_POPULAR_TV.category  to "most_popular_tv",
+        MainCategoryApi.IN_THEATERS.category  to "in_theaters"
     )
-
 
     fun getCategoryIds(): Map<String,String>{
         return categoryIdMap
--- app/src/main/java/ch/mov/cinema/cinemaapp/model/enums/MainCategoryApi.kt
+++ app/src/main/java/ch/mov/cinema/cinemaapp/model/enums/MainCategoryApi.kt
@@ -1,11 +1,9 @@
 package ch.mov.cinema.cinemaapp.model.enums
 
 enum class MainCategoryApi (val category: String){
-    COMING_SOON("https://imdb-api.com/en/API/ComingSoon/k_dpwxdc3v"),
-    TOP_250_MOVIES("https://imdb-api.com/en/API/Top250TVs/k_dpwxdc3v"),
-    MOST_POPULAR_MOVIES("https://imdb-api.com/en/API/MostPopularMovies/k_dpwxdc3v"),
-    MOST_POPULAR_TV("https://imdb-api.com/en/API/MostPopularTVs/k_dpwxdc3v"),
-    IN_THEATERS("https://imdb-api.com/en/API/InTheaters/k_dpwxdc3v"),
-    BOX_OFFICE("https://imdb-api.com/en/API/BoxOffice/k_dpwxdc3v"),
-    BOX_OFFICE_ALL_TIME("https://imdb-api.com/en/API/BoxOfficeAllTime/k_dpwxdc3v")
+    COMING_SOON("https://imdb-api.com/en/API/ComingSoon/k_j38getry"),
+    TOP_250_MOVIES("https://imdb-api.com/en/API/Top250Movies/k_j38getry"),
+    MOST_POPULAR_MOVIES("https://imdb-api.com/en/API/MostPopularMovies/k_j38getry"),
+    MOST_POPULAR_TV( "https://imdb-api.com/en/API/MostPopularTVs/k_j38getry"),
+    IN_THEATERS("https://imdb-api.com/en/API/InTheaters/k_j38getry"),
 }
